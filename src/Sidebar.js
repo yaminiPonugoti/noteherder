432a16d2e71e2d7e0e1c12baf0cc381ef24c0102
Yamini Ponugoti <yaminiponugoti@yaminis-mbp.coldspring.marian.edu>
2018-05-22 15:29:21 -0400

    Begin styling

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,57 @@
 import React from 'react'
 
+import quill from './quill.svg'
+import newIcon from './new.png'
+import newHover from './new-hover.png'
+
 const Sidebar = () => {
     return(
-        <div className = "Sidebar">
-            Sidebar
-        </div>
+        <nav 
+            className="Sidebar"
+            style = {styles.sidebar}
+        >
+          <div 
+            className="logo"
+            style = {styles.logo}
+          >
+            <img 
+                src={quill} 
+                alt="Noteherder"
+                style = {styles.logoImg} 
+            />
+          </div>
+          <a class="new-note" href="/notes">
+            <img src={newHover} alt="New note" />
+            <img class="outline" src={newIcon} alt="New note" />
+          </a>
+          <div class="SignOut">
+            <button>
+              <i class="fa fa-sign-out"></i>
+            </button>
+          </div>
+        </nav>
     )
+}
+
+const styles = {
+    sidebar: {
+        width: '6rem',
+        backgroundColor: '#f3f3f3',
+        padding: '0.5rem 0',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    logo: {
+        fontFamily: '"Fauna One"',
+        color: '#666',
+        fontSize: '3rem',
+    },
+    logoImg: {
+        width: '3rem',
+        paddingLeft: '0.4rem',
+    }
+
 }
 
 export default Sidebar
