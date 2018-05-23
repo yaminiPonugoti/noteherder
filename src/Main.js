c29a87be0bb412a4ea6d040f8bc33e82995db644
Yamini Ponugoti <yaminiponugoti@yaminis-mbp.coldspring.marian.edu>
2018-05-23 15:20:05 -0400

    Load the current note into NoteForm

--- src/Main.js
+++ src/Main.js
@@ -1,55 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react'
 
 import Sidebar from './Sidebar'
 import NoteList from './NoteList'
 import NoteForm from './NoteForm'
 
-class Main extends React.Component{
-    constructor(){
-        super()
-        this.state = {
-            currentNote: null,
-            notes: [
-              {
-                id: 1,
-                title: 'JS thoughts',
-                body: 'I just love JS so much.',
-              },
-              {
-                id: 2,
-                title: 'Breakfast',
-                body: 'FOR IT!',
-              },
-              {
-                id: 3,
-                title: 'Class notes',
-                body: 'React is spiffy',
-              }
-            ],
-        }
+class Main extends Component {
+  constructor() {
+    super()
+    this.state = {
+      currentNote: {
+        id: null,
+        title: '',
+        body: '',
+      },
+      notes: [
+        {
+          id: 1,
+          title: 'JS thoughts',
+          body: 'I just love JS so much.',
+        },
+        {
+          id: 2,
+          title: 'Breakfast',
+          body: 'FOR IT!',
+        },
+      ],
     }
+  }
 
-    setCurrentNote(note){
-        this.setState({currentNote: note})
-    }
-    render(){
-        return(
-            <div 
-            className="Main"
-            style={style}
-            >
-                <Sidebar />
-                <NoteList notes={this.state.notes} setCurrentNote={this.setCurrentNote} />
-                <NoteForm />
-             </div>
+  setCurrentNote = (note) => {
+    this.setState({ currentNote: note })
+  }
+
+  render() {
+    return (
+      <div
+        className="Main"
+        style={style}
+      >
+        <Sidebar />
+        <NoteList
+          notes={this.state.notes}
+          setCurrentNote={this.setCurrentNote}
+        />
+        <NoteForm currentNote={this.state.currentNote} />
+      </div>
     )
-    }
+  }
 }
 
 const style = {
-    display: 'flex',
-    height: '100vh',
-    alignItems: 'stretch',
+  display: 'flex',
+  height: '100vh',
+  alignItems: 'stretch',
 }
 
 export default Main
