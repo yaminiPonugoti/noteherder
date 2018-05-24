16178431c7df994af0b968965c664218681bed7d
Yamini Ponugoti <yaminiponugoti@yaminis-mbp.coldspring.marian.edu>
2018-05-24 17:04:37 -0400

    Add Github authentication

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,13 +1,36 @@
 import React from 'react'
 
-const SignIn = ({ handleAuth }) => {
+import './SignIn.css'
+import quill from './quill.svg'
+import googleLogo from './google.svg'
+import { auth, githubProvider } from './base'
+
+const SignIn = () => {
+  const authenticate = (provider) => {
+    auth.signInWithPopup(provider)
+  }
+
   return (
     <div className="SignIn">
-      <button
-        onClick={handleAuth}
-      >Sign In</button>
+      <header className="Header">
+        <img src={quill} alt="" />
+        <span className="title">Noteherder</span>
+      </header>
+      <main>
+        <h3>Hey, Nerd! You Like Notes?</h3>
+        <p>You never know when you'll need to write crap down. In fact, you should probably be taking notes right now.</p>
+        <button
+          className="github"
+          onClick={() => authenticate(githubProvider)}>
+          <i className="fab fa-github"></i>
+          Sign in with GitHub
+        </button>
+        <button className="google">
+          <img src={googleLogo} alt="" />
+          Sign in with Google
+        </button>
+      </main>
     </div>
   )
 }
-
 export default SignIn
--- src/base.js
+++ src/base.js
@@ -1,13 +1,20 @@
 import firebase from 'firebase/app'
 import 'firebase/database'
+import 'firebase/auth'
 import Rebase from 're-base'
-const config = {
-    apiKey: "AIzaSyCeV2hyefSc2NCrTMaK_6pfpZc0CcvOwwE",
-    authDomain: "noteherder-b96ed.firebaseapp.com",
-    databaseURL: "https://noteherder-b96ed.firebaseio.com",
-    projectId: "noteherder-b96ed",
-    storageBucket: "",
-    messagingSenderId: "685444276891"
-  };
-const app = firebase.initializeApp(config);
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCeV2hyefSc2NCrTMaK_6pfpZc0CcvOwwE",
+  authDomain: "noteherder-b96ed.firebaseapp.com",
+  databaseURL: "https://noteherder-b96ed.firebaseio.com",
+  projectId: "noteherder-b96ed",
+  storageBucket: "noteherder-b96ed.appspot.com",
+  messagingSenderId: "685444276891"
+};
+const app = firebase.initializeApp(config)
+
+export const githubProvider = new firebase.auth.GithubAuthProvider()
+export const auth = firebase.auth()
+
 export default Rebase.createClass(app.database())
