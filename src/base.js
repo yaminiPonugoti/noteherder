f2f9dfbff4a16f7f5d2bcc589141bb9a1aed8fbd
Yamini Ponugoti <yaminiponugoti@yaminis-mbp.coldspring.marian.edu>
2018-05-29 13:42:18 -0400

    Google authentication in base.js

--- src/base.js
+++ src/base.js
@@ -17,4 +17,6 @@ const app = firebase.initializeApp(config)
 export const githubProvider = new firebase.auth.GithubAuthProvider()
 export const auth = firebase.auth()
 
+export const googleProvider = new firebase.auth.GoogleAuthProvider()
+
 export default Rebase.createClass(app.database())
