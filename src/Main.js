8f2e90f51f9d09000d3acf4db77e386fe146ec0a
Yamini Ponugoti <yponugot@purdue.edu>
2018-05-30 14:27:57 -0400

    Add route that includes note ID

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import { Switch, Route } from 'react-router-dom'
 import base from './base'
 import Sidebar from './Sidebar'
 import NoteList from './NoteList'
@@ -67,6 +67,11 @@ class Main extends Component {
   }
 
   render() {
+    const formProps = {
+      currentNote: this.state.currentNote,
+      saveNote: this.saveNote,
+      removeCurrentNote: this.removeCurrentNote
+    }
     return (
       <div
         className="Main"
@@ -80,11 +85,21 @@ class Main extends Component {
           notes={this.state.notes}
           setCurrentNote={this.setCurrentNote}
         />
-        <NoteForm
-          currentNote={this.state.currentNote}
-          saveNote={this.saveNote}
-          removeCurrentNote={this.removeCurrentNote}
-        />
+        <Switch>
+          <Route 
+            path="/notes/:id"
+            render={navProps => (<NoteForm
+             {...formProps}
+             {...navProps}
+            />)} 
+          />
+          <Route 
+            render={navProps => (<NoteForm
+              {...formProps}
+              {...navProps}
+            />)} 
+          />
+        </Switch>
       </div>
     )
   }
